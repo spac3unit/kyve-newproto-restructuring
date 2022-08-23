6a615bd0b7f0099ef39e8b9ced674dc25ab1a5a7
Troy Kessler <troy.kessler99@gmail.com>
2022-08-23 13:42:28 +0200

    finished new unit tests for can_propose and msg_submit_bundle_proposal

--- x/query/keeper/grpc_query_can_propose_test.go
+++ x/query/keeper/grpc_query_can_propose_test.go
@@ -327,8 +327,173 @@ var _ = Describe("Can Propose Tests", Ordered, func() {
 		Expect(txErr.Error()).To(Equal(canPropose.Reason))
 	})
 
-	// TODO: not designated uploader
-	// TODO: upload interval not reached
-	// TODO: invalid from_height
-	// TODO: can actually propose
+	It("Can propose should fail if proposer is not designated uploader", func() {
+		// ACT
+		canPropose, err := s.App().QueryKeeper.CanPropose(sdk.WrapSDKContext(s.Ctx()), &querytypes.QueryCanProposeRequest{
+			PoolId:     0,
+			Staker:     i.STAKER_0,
+			Proposer:   i.VALADDRESS_0,
+			FromHeight: 100,
+		})
+
+		// ASSERT
+		Expect(err).To(BeNil())
+
+		Expect(canPropose.Possible).To(BeFalse())
+		Expect(canPropose.Reason).To(Equal(sdkErrors.Wrapf(bundletypes.ErrNotDesignatedUploader, "expected %v received %v", i.STAKER_1, i.STAKER_0).Error()))
+
+		_, txErr := s.RunTxBundles(&bundletypes.MsgSubmitBundleProposal{
+			Creator:    i.VALADDRESS_0,
+			Staker:     i.STAKER_0,
+			PoolId:     0,
+			StorageId:  "test_storage_id",
+			ByteSize:   100,
+			FromHeight: 100,
+			ToHeight:   200,
+			FromKey:    "99",
+			ToKey:      "199",
+			ToValue:    "test_value",
+			BundleHash: "test_hash",
+		})
+
+		Expect(txErr).NotTo(BeNil())
+		Expect(txErr.Error()).To(Equal(canPropose.Reason))
+	})
+
+	It("Can propose should fail if proposed before upload interval", func() {
+		// ARRANGE
+		pool, _ := s.App().PoolKeeper.GetPool(s.Ctx(), 0)
+		// increase upload interval for upload timeout
+		pool.UploadInterval = 120
+
+		s.App().PoolKeeper.SetPool(s.Ctx(), pool)
+
+		// ACT
+		canPropose, err := s.App().QueryKeeper.CanPropose(sdk.WrapSDKContext(s.Ctx()), &querytypes.QueryCanProposeRequest{
+			PoolId:     0,
+			Staker:     i.STAKER_1,
+			Proposer:   i.VALADDRESS_1,
+			FromHeight: 100,
+		})
+
+		// ASSERT
+		Expect(err).To(BeNil())
+
+		bundleProposal, _ := s.App().BundlesKeeper.GetBundleProposal(s.Ctx(), 0)
+
+		Expect(canPropose.Possible).To(BeFalse())
+		Expect(canPropose.Reason).To(Equal(sdkErrors.Wrapf(bundletypes.ErrUploadInterval, "expected %v < %v", s.Ctx().BlockTime().Unix(), bundleProposal.CreatedAt+pool.UploadInterval).Error()))
+
+		_, txErr := s.RunTxBundles(&bundletypes.MsgSubmitBundleProposal{
+			Creator:    i.VALADDRESS_1,
+			Staker:     i.STAKER_1,
+			PoolId:     0,
+			StorageId:  "test_storage_id",
+			ByteSize:   100,
+			FromHeight: 100,
+			ToHeight:   200,
+			FromKey:    "99",
+			ToKey:      "199",
+			ToValue:    "test_value",
+			BundleHash: "test_hash",
+		})
+
+		Expect(txErr).NotTo(BeNil())
+		Expect(txErr.Error()).To(Equal(canPropose.Reason))
+	})
+
+	It("Can propose should fail if invalid from_height", func() {
+		// ACT
+		canPropose_1, err_1 := s.App().QueryKeeper.CanPropose(sdk.WrapSDKContext(s.Ctx()), &querytypes.QueryCanProposeRequest{
+			PoolId:     0,
+			Staker:     i.STAKER_1,
+			Proposer:   i.VALADDRESS_1,
+			FromHeight: 99,
+		})
+
+		canPropose_2, err_2 := s.App().QueryKeeper.CanPropose(sdk.WrapSDKContext(s.Ctx()), &querytypes.QueryCanProposeRequest{
+			PoolId:     0,
+			Staker:     i.STAKER_1,
+			Proposer:   i.VALADDRESS_1,
+			FromHeight: 101,
+		})
+
+		// ASSERT
+		Expect(err_1).To(BeNil())
+		Expect(err_2).To(BeNil())
+
+		bundleProposal, _ := s.App().BundlesKeeper.GetBundleProposal(s.Ctx(), 0)
+
+		Expect(canPropose_1.Possible).To(BeFalse())
+		Expect(canPropose_1.Reason).To(Equal(sdkErrors.Wrapf(types.ErrFromHeight, "expected %v received %v", bundleProposal.ToHeight, 99).Error()))
+
+		Expect(canPropose_2.Possible).To(BeFalse())
+		Expect(canPropose_2.Reason).To(Equal(sdkErrors.Wrapf(types.ErrFromHeight, "expected %v received %v", bundleProposal.ToHeight, 101).Error()))
+
+		_, txErr_1 := s.RunTxBundles(&bundletypes.MsgSubmitBundleProposal{
+			Creator:    i.VALADDRESS_1,
+			Staker:     i.STAKER_1,
+			PoolId:     0,
+			StorageId:  "test_storage_id",
+			ByteSize:   100,
+			FromHeight: 99,
+			ToHeight:   199,
+			FromKey:    "99",
+			ToKey:      "199",
+			ToValue:    "test_value",
+			BundleHash: "test_hash",
+		})
+
+		Expect(txErr_1).NotTo(BeNil())
+		Expect(txErr_1.Error()).To(Equal(canPropose_1.Reason))
+
+		_, txErr_2 := s.RunTxBundles(&bundletypes.MsgSubmitBundleProposal{
+			Creator:    i.VALADDRESS_1,
+			Staker:     i.STAKER_1,
+			PoolId:     0,
+			StorageId:  "test_storage_id",
+			ByteSize:   100,
+			FromHeight: 101,
+			ToHeight:   199,
+			FromKey:    "99",
+			ToKey:      "199",
+			ToValue:    "test_value",
+			BundleHash: "test_hash",
+		})
+
+		Expect(txErr_2).NotTo(BeNil())
+		Expect(txErr_2.Error()).To(Equal(canPropose_2.Reason))
+	})
+
+	It("Can propose should succeed", func() {
+		// ACT
+		canPropose, err := s.App().QueryKeeper.CanPropose(sdk.WrapSDKContext(s.Ctx()), &querytypes.QueryCanProposeRequest{
+			PoolId:     0,
+			Staker:     i.STAKER_1,
+			Proposer:   i.VALADDRESS_1,
+			FromHeight: 100,
+		})
+
+		// ASSERT
+		Expect(err).To(BeNil())
+
+		Expect(canPropose.Possible).To(BeTrue())
+		Expect(canPropose.Reason).To(BeEmpty())
+
+		_, txErr := s.RunTxBundles(&bundletypes.MsgSubmitBundleProposal{
+			Creator:    i.VALADDRESS_1,
+			Staker:     i.STAKER_1,
+			PoolId:     0,
+			StorageId:  "test_storage_id",
+			ByteSize:   100,
+			FromHeight: 100,
+			ToHeight:   200,
+			FromKey:    "99",
+			ToKey:      "199",
+			ToValue:    "test_value",
+			BundleHash: "test_hash",
+		})
+
+		Expect(txErr).To(BeNil())
+	})
 })
